805280b4d9f668b411ea71c30e4062345af75025
alyssa-gerhart <alyssa.gerhart20@my.benedict.edu>
2023-02-02 12:47:58 -0500

    Added a profile page

---
+++ src/profile/alyssa-gerhart.js
@@ -0,0 +1,35 @@
+function getStudents() {
+    const studentsApiUrl = "http://localhost:4000/students?slug=alyssa-gerhart";
+    return fetch(studentsApiUrl, {
+      method: "GET"
+    })
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => console.error(error));
+  }
+  
+document.addEventListener("DOMContentLoaded", async () => {
+ const students = await getStudents();
+  
+    const studentContainer = document.getElementById("student-cards-container");
+  
+    let content = "";
+    students.forEach((student) => {
+      content += `
+      <div class="card linked">
+        <div class="card-header">
+          <img class="card-avatar" src="alyssa-avatar.JPG" />
+          <div>
+            <a href="/profile/${student.slug}" class="card-title link">${student.name}</a>
+            <p class="card-subtitle line-1">
+              ${student.description1}
+            </p>
+            <p class="card-subtitle line-2">${student.description2}</p>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+  
+    studentContainer.innerHTML = content;
+  });
