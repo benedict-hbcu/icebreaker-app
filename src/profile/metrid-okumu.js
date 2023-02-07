69ff371f0e20d0a78dfac402c85cd1113f3a0a18
Metrid Okumu <metrid.okumu99@my.benedict.edu>
2023-02-07 13:46:05 -0500

    My  added the nasa image to my background

--- src/profile/metrid-okumu.js
+++ src/profile/metrid-okumu.js
@@ -10,7 +10,7 @@ function getStudents() {
       .catch((error) => console.error(error));
 }
 function getNasaBanner(dob) { 
-    const NasaApiUrl = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=1999-01-02' //remove hard code
+    const NasaApiUrl = ` https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2022-${dob}` 
     return fetch(NasaApiUrl, {
       method: "GET"
     })
@@ -24,31 +24,35 @@ function getNasaBanner(dob) {
     console.log(student)
 
     const dob = student[0].dob;
+    const date=dob.split('-')
+    const date2=date[1]+"-"+date[2]
     console.log(dob)
 
-    const nasaBanner= await getNasaBanner(dob);
+    const nasaBanner= await getNasaBanner(date2);
      nasaBanner.url
     console.log(nasaBanner.url)
     
-
+// how to get the picture showing 
+   document.getElementById("nasaBanner").style.backgroundImage=`url('${nasaBanner.url}')`;
     const studentContainer = document.getElementById("student-cards-container");
   
    
       content = `
-      <div class="card linked">
-        <div class="card-header">
-          <img class="card-avatar" src="${student[0].avatar}" />
-          <div>
-            <a href="/profile/${student[0].slug}" class="card-title link">${student[0].name}</a>
-            <p class="card-subtitle line-1">
-              ${student[0].description1}
-            </p>
-            <p class="card-subtitle line-2">${student[0].description2}</p>
+      <div src= "${nasaBanner.url}">
+        <div class="card linked">
+          <div class="card-header">
+            <img class="card-avatar" src="../${student[0].avatar}" />
+            <div>
+              <a href="/profile/${student[0].slug}" class="card-title link">${student[0].name}</a>
+              <p class="card-subtitle line-1">
+                ${student[0].description1}
+              </p>
+              <p class="card-subtitle line-2">${student[0].description2}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </div>  
       `;
-    
   
     studentContainer.innerHTML = content;
   
