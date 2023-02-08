026b59ba0d643162cc5130b8781a91317debc86d
alyssa-gerhart <alyssa.gerhart20@my.benedict.edu>
2023-02-08 18:01:13 -0500

    Undid the manual insertion

--- src/profile/alyssa-gerhart.js
+++ src/profile/alyssa-gerhart.js
@@ -1,44 +1,53 @@
-function getStudents() {
-    const studentsApiUrl = "http://localhost:4000/students?slug=alyssa-gerhart";
-    return fetch(studentsApiUrl, {
-      method: "GET"
-    })
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((error) => console.error(error));
+async function getAlyssa() {
+  const AlyssaApiUrl = "http://localhost:4000/students?slug=alyssa-gerhart";
+  try {
+      const response = await fetch(AlyssaApiUrl, {
+          method: "GET"
+      });
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      return console.error(error);
   }
-// add.addEventListener(function getImage(){
-//   fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2022-03-05")
-//   .then(res => res.json())
-//   .then(result =>{
-//       console.log(result)
-//       Image.src = result.message
-//   })
-//   .catch(err=>console.log(err))
-// })
-
-  
+}
+async function getNasaBackground(date) {
+try {
+  const nasaApiUrl = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2013-${date}`
+  const response = await fetch(nasaApiUrl, {
+    method: "GET"
+  });
+  const data2 = await response.json();
+  return data2;
+} catch (error) {
+  return console.error(error);
+}
+}
 document.addEventListener("DOMContentLoaded", async () => {
- const students = await getStudents();
-  
-    const studentContainer = document.getElementById("student-cards-container");
-  
-    let content = "";
-    students.forEach((student) => {
-      content += `
-      <div class="card linked">
-        <div class="card-header">
-          <img class="card-avatar" src="alyssa-avatar.JPG" />
-          <div>
-            <p class="card-subtitle line-1">
-              ${student.description1}
-            </p>
-            <p class="card-subtitle line-2">${student.description2}</p>
-          </div>
+const students = await getAlyssa();
+const alyssaProfile = document.getElementById("alyssa-profile");
+const student = students[0]
+const date = student.dob.split('-');
+const dateSplit = date[1]+"-"+date[2];
+const nasaImg = await getNasaBackground(dateSplit);
+
+document.getElementById("card-outer").style.backgroundImage=`url('${nasaImg.url}')`;
+
+const content = `
+  <div background-size: src= "${nasaImg.url}">
+    <div class="card linked">
+      <div class="card-header">
+        <img class="card-avatar" src="../${student.avatar}" />
+        <div>
+        <p class="card-title link">${student.name}</p>
+          <p class="card-subtitle line-1">
+            ${student.description1}
+          </p>
+          <p class="card-subtitle line-2">${student.description2}</p>
         </div>
       </div>
-      `;
-    });
-  
-    studentContainer.innerHTML = content;
-  });
+    </div>
+  </div>  
+  `;
+
+alyssaProfile.innerHTML = content;
+});
